7911326a082b2f9bcac11db609ec15b90445a314
Muhamad Wisnu Hardiantoro <m.wisnu.h236@gmail.com>
2024-08-21 15:16:01 +0700

    Update program.ino

--- 024-017-Dipa/program/program.ino
+++ 024-017-Dipa/program/program.ino
@@ -13,9 +13,9 @@ void setup() {
 }
 
 void loop() {
-  // if (Serial.available() > 0) {
-  //   temperatureTest = Serial.readStringUntil('\n').toFloat();
-  // }
+  if (Serial.available() > 0) {
+    temperatureTest = Serial.readStringUntil('\n').toFloat();
+  }
 
   int state = !digitalRead(BUTTON_PIN);
   if (state) {
@@ -46,12 +46,12 @@ void loop() {
       prevMillis = currentMillis;
       DS18B20.requestTemperatures();
 
-      temperature = DS18B20.getTempCByIndex(0);
-      // temperature = temperatureTest + (temperatureTest * 0.018 * ((float)random(-100, 100) / 100.0));
+      //temperature = DS18B20.getTempCByIndex(0);
+      temperature = temperatureTest + (temperatureTest * 0.018 * ((float)random(-100, 100) / 100.0));
 
       outputPID = map(outputPID, 0, 100, 0, 255);
       analogWrite(SSR_PIN, outputPID);
-      analogWrite(SSR_PIN, 255);
+      // analogWrite(SSR_PIN, 255);
 
       lcd.setCursor(0, 0);
       lcd.print("PV =             ");
